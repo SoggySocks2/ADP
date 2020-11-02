1051ea1c61f6f9439de2698cbc340ccefeddbf35
Peter Jones <peter.jones@loyaltylogistix.com>
2020-11-02 17:29:19 +0000

    minor page mods

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,23 +1,60 @@
 import React from 'react';
 import './gallery.css';
-import Card from '../components/card/card';
-import cardImage1 from '../images/20200808_154800-768x576.jpg';
-import cardImage2 from '../images/20200730_115221-768x576.jpg';
-import cardImage3 from '../images/20200829_152225-768x576.jpg';
-import cardImage4 from '../images/20200904_155742-768x576.jpg';
-import cardImage5 from '../images/20200829_132900-768x1024.jpg';
+// import Card from '../components/card/card';
+import Mountains from '../images/mountains.jpg';
+import Lights from '../images/lights.jpg';
+import Nature from '../images/nature.jpg';
+import Bear from '../images/p3.jpg';
 
 class Gallery extends React.Component {
     render() {
         return (
-            <>
-                <h1>Gallery</h1>
-                <Card src={cardImage1} alt="Gallery Image 1" width="100%" title="" content="" />
-                <Card src={cardImage2} alt="Gallery Image 2" width="100%" title="" content="" />
-                <Card src={cardImage3} alt="Gallery Image 3" width="100%" title="" content="" />
-                <Card src={cardImage4} alt="Gallery Image 4" width="100%" title="" content="" />
-                <Card src={cardImage5} alt="Gallery Image 5" width="100%" title="" content="" />
-            </>
+            /* MAIN (Center website) */
+            <div className="main">
+
+                <h2>Gallery</h2>
+                <p>Resize the browser window to see the responsive effect.</p>
+
+                {/* Portfolio Gallery Grid */}
+                <div className="rowName">
+                    <div className="column">
+                        <div className="content">
+                            <img src={Mountains} alt="Mountains" width="100%" />
+                            <h3>My Work</h3>
+                            <p>Lorem ipsum..</p>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div className="content">
+                            <img src={Lights} alt="Lights" width="100%" />
+                            <h3>My Work</h3>
+                            <p>Lorem ipsum..</p>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div className="content">
+                            <img src={Nature} alt="Nature" width="100%" />
+                            <h3>My Work</h3>
+                            <p>Lorem ipsum..</p>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div className="content">
+                            <img src={Mountains} alt="Mountains" width="100%" />
+                            <h3>My Work</h3>
+                            <p>Lorem ipsum..</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="content">
+                    <img src={Bear} alt="Bear" width="100%" />
+                    <h3>Some Other Work</h3>
+                    <p>Lorem ipsum..</p>
+                </div>
+
+            {/* <!-- END MAIN --> */}
+            </div>
         );
     }
 }
